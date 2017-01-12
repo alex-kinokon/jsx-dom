7f636faf6806f58dabf32b04696001ac6eb1d0b4
alexlur <alexlur@fastmail.fm>
2017-01-12 00:29:30 -0500

    Fix a bug with SVG className

--- src/index.ts
+++ src/index.ts
@@ -99,7 +99,7 @@ function attributes( attr, node ) {
         continue;
       case 'class':
       case 'className':
-        node.className = className( value );
+        node.setAttribute('class', className( value ));
         continue;
     }
 
