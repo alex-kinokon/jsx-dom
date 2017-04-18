e1204b54b414f3bfdcdf1bac43dabc9745c99f8f
alex-kinokon <8125011+alex-kinokon@users.noreply.github.com>
2017-04-17 21:12:57 -0400

    4.2.0: Supports for boolean attr. Adds tests.

--- test/test.tsx
+++ test/test.tsx
@@ -9,6 +9,24 @@ const window = global['window'] = document.defaultView;
 
 const jsx = require('../dist/index.cjs.js');
 
+// Monkey patch jsdom to support dataset.
+function toPropKey(prop) {
+	return 'data-' + `${prop}`.toLowerCase();
+}
+Object.defineProperty((window as any).Element.prototype, 'dataset', {
+	get() {
+		return new Proxy(this, {
+			get(target, prop, receiver) {
+				return target.getAttribute(toPropKey(prop));
+			},
+			set(target, prop, value, receiver) {
+				target.setAttribute(toPropKey(prop), value);
+				return true;
+			}
+		});
+	}
+});
+
 describe('jsx-dom', function () {
 
 	it('creates a <div> element', function () {
@@ -38,8 +56,19 @@ describe('jsx-dom', function () {
 
 	});
 
-  it('supports boolean attributes', function () {
-		expect((<input disabled={true} />).getAttribute("disabled")).to.equal("");
-		expect((<input disabled={false} />).getAttribute("disabled")).to.equal(null);
+	describe('attributes', function () {
+		it('supports boolean attributes', function () {
+			expect((<input disabled={true} />).getAttribute("disabled")).to.equal("");
+			expect((<input disabled={false} />).getAttribute("disabled")).to.equal(null);
+		});
+
+		it('supports dataset', function () {
+			expect((<div data-key="value" />).dataset.key).to.equal("value");
+			expect((<div dataset={{ key: "value" }} />).getAttribute('data-key')).to.equal("value");
+		});
+	});
+
+	it('supports event listeners', function (done) {
+		(<button onClick={() => done()} />).click();
 	});
 });
