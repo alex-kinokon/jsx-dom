34af0825e054def74376fedd9e52bde8f266ea3f
alex-kinokon <8125011+alex-kinokon@users.noreply.github.com>
2017-11-29 15:07:00 -0500

    Adds test for JSX Fragment

--- test/pretest.ts
+++ test/pretest.ts
@@ -1,31 +1,32 @@
-const { JSDOM } = require('jsdom');
+import { JSDOM } from 'jsdom';
 
 // Set up jsdom
 const dom = new JSDOM('');
 (global as any).window = dom.window;
 (global as any).document = dom.window.document;
+(global as any).Node = (window as any).Node;
 
 // Monkey patch jsdom to support dataset.
 function toPropKey(prop: PropertyKey) {
-	return 'data-' + `${prop}`.toLowerCase();
+  return 'data-' + `${prop}`.toLowerCase();
 }
 
 Object.defineProperties((window as any).Element.prototype, {
-	innerText: {
-		get() {	return this.textContent; },
-		set(value: string) { this.textContent = value; }
-	},
-	dataset: {
-		get() {
-			return new Proxy(this, {
-				get(target, prop) {
-					return target.getAttribute(toPropKey(prop));
-				},
-				set(target, prop, value) {
-					target.setAttribute(toPropKey(prop), value);
-					return true;
-				}
-			});
-		}
-	},
+  innerText: {
+    get() {  return this.textContent; },
+    set(value: string) { this.textContent = value; }
+  },
+  dataset: {
+    get() {
+      return new Proxy(this, {
+        get(target, prop) {
+          return target.getAttribute(toPropKey(prop));
+        },
+        set(target, prop, value) {
+          target.setAttribute(toPropKey(prop), value);
+          return true;
+        }
+      });
+    }
+  },
 });
