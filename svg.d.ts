76d4770893b2faaaf1b0d47daa11a9b1a794b9e9
alex-kinokon <8125011+alex-kinokon@users.noreply.github.com>
2020-03-15 20:14:55 -0400

    Fix svg.d.ts

--- svg.d.ts
+++ svg.d.ts
@@ -1,8 +1,1 @@
-/**
- * Adapted from React TypeScript definition from DefinitelyTyped.
- * @see https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/react/index.d.ts
- */
-
-declare module "jsx-dom/svg" {
-  export * from "jsx-dom"
-}
+export * from "./index"
