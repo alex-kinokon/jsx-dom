2cbbf1ab9ba57d42b11fe5b29c54d56c8b764155
alex-kinokon <8125011+alex-kinokon@users.noreply.github.com>
2021-03-20 16:47:53 -0400

    Update files

---
+++ test/test-automatic-runtime.tsx
@@ -0,0 +1,72 @@
+import { writeFile, remove, ensureDir } from "fs-extra"
+import { resolve } from "path"
+import { expect } from "chai"
+import { rollup } from "rollup"
+import babel from "@rollup/plugin-babel"
+import { formatSource as formatCode } from "../scripts/format-source"
+
+const TEMP_FOLDER = "./temp"
+describe("Automatic Runtime", () => {
+  before(async () => {
+    await ensureDir(TEMP_FOLDER)
+  })
+  after(async () => {
+    await remove(TEMP_FOLDER)
+  })
+
+  async function generateBundle(code: string, babelOptions?: Record<string, any>) {
+    const entryPath = resolve(TEMP_FOLDER, "index.js")
+    await writeFile(entryPath, formatCode(code))
+    const build = await rollup({
+      input: entryPath,
+      external: path => path.startsWith("jsx-dom"),
+      context: "this",
+      plugins: [
+        babel({
+          babelrc: false,
+          babelHelpers: "inline",
+          presets: [
+            [
+              "@babel/preset-react",
+              { runtime: "automatic", importSource: "jsx-dom", ...babelOptions },
+            ],
+          ],
+        }),
+      ],
+    })
+    const { output } = await build.generate({})
+    expect(output).to.have.lengthOf(1)
+    return formatCode(output[0].code)
+  }
+
+  generateBundle.options = (options: Record<string, any>) => (code: string) =>
+    generateBundle(code, options)
+
+  it("should correctly import jsx functions", async () => {
+    const bundle = await generateBundle(/* javascript */ `
+      render(<div />)
+    `)
+    expect(bundle).to.equalIgnoreSpaces(/* javascript */ `
+      import { jsx } from "jsx-dom/jsx-runtime"
+      render(jsx("div", {}))
+    `)
+  })
+
+  it("should correctly import jsx functions in development mode", async () => {
+    const generate = generateBundle.options({ development: true })
+    const bundle = await generate(/* javascript */ `
+      render(<div />)
+    `)
+
+    expect(bundle).to.startWith('import { jsxDEV } from "jsx-dom/jsx-dev-runtime"')
+    expect(bundle).to.containIgnoreSpaces(/* javascript */ `
+      render(
+        jsxDEV("div", {}, void 0, false, { 
+          fileName: _jsxFileName,
+          lineNumber: 1,
+          columnNumber: 8,
+        }, this)
+      )
+    `)
+  })
+})
