3cf50a2272368dd3b23f73cd1176c508a30d3e16
proteriax <8125011+proteriax@users.noreply.github.com>
2021-03-20 16:47:53 -0400

    Update files

--- scripts/build.ts
+++ scripts/build.ts
@@ -6,6 +6,7 @@ import replace, { RollupReplaceOptions } from "@rollup/plugin-replace"
 import prettier from "rollup-plugin-prettier"
 import node from "@rollup/plugin-node-resolve"
 import { rollup } from "rollup"
+import { formatSource } from "./format-source"
 
 const extensions = [".ts", ".js"]
 const jsxRuntimeExports = ["jsx", "jsxs", "Fragment"]
@@ -13,30 +14,21 @@ const jsxRuntimeExports = ["jsx", "jsxs", "Fragment"]
 const OUT_DIR = resolve(__dirname, "../build")
 const OUT_DIR_MIN = resolve(OUT_DIR, "./min")
 
-type NamedExport = string | [string, string]
-
 interface BuildOptions {
   input: string
   outputDir: string
   inject: RollupReplaceOptions
 }
 
-const prepareOptions = function (name: string, options?: Partial<BuildOptions>) {
-  return {
-    input: `./src/${name}.ts`,
-    outputDir: OUT_DIR,
-    inject: {},
-    ...options,
-  } as BuildOptions
-}
-
-const $build = async function (name: string, options?: Partial<BuildOptions>) {
-  options = prepareOptions(name, options)
-
+const buildRollup = async function (
+  name: string,
+  { outputDir = OUT_DIR, inject = {} }: Partial<BuildOptions>
+) {
   const bundle = await rollup({
-    input: options.input,
+    input: `./src/${name}.ts`,
+    external: ["jsx-dom", "jsx-dom/min"],
     plugins: [
-      replace(options.inject),
+      replace({ ...inject, preventAssignment: true }),
       babel({
         extensions,
         comments: false,
@@ -63,17 +55,14 @@ const $build = async function (name: string, options?: Partial<BuildOptions>) {
     ],
   })
 
-  await Promise.all([
-    bundle.write({
-      format: "cjs",
-      file: `${options.outputDir}/${name}.cjs.js`,
-      exports: "named",
-    }),
-    bundle.write({ format: "es", file: `${options.outputDir}/${name}.js`, exports: "named" }),
-  ])
+  await bundle.write({
+    format: "es",
+    file: `${outputDir}/${name}.js`,
+    exports: "named",
+  })
 }
 
-async function copyPackage() {
+async function copyPackageJson() {
   const source = await fs.readJSON(resolve(__dirname, "../package.json"))
   delete source.devDependencies
   delete source.scripts
@@ -81,46 +70,41 @@ async function copyPackage() {
   await fs.writeJSON(resolve(OUT_DIR, "package.json"), source, { spaces: 2 })
 }
 
-async function provideReexport(
-  name: string,
-  dest: string,
-  src: string,
-  namedExports?: NamedExport[]
-) {
-  const imports = namedExports
-    ? `{\n${namedExports
-        .map((x) => `  ${Array.isArray(x) ? `${x[0]} as ${x[1]}` : x},\n`)
-        .join("")}}`
-    : "*"
+async function reexport(name: string, dest: string, src: string, namedExports?: string[]) {
+  const imports = namedExports ? `{${namedExports.join(",")}}` : "*"
   const content = `export ${imports} from "${src}"`
 
   await fs.mkdirp(dest)
-  await fs.writeFile(resolve(dest, name), content)
+  await fs.writeFile(resolve(dest, name), formatSource(content))
 }
 
 // https://github.com/jprichardson/node-fs-extra/issues/323
 const copy = (src: string, dest: string) => fs.copy(src, resolve(dest, src))
 
 export async function build() {
+  await fs.remove(OUT_DIR)
   await fs.ensureDir(OUT_DIR)
   await Promise.all([
-    copyPackage(),
+    copyPackageJson(),
     copy("index.d.ts", OUT_DIR),
     copy("README.md", OUT_DIR),
     copy("CHANGELOG.md", OUT_DIR),
     copy("LICENSE", OUT_DIR),
+    fs.writeFile(resolve(OUT_DIR, "jsx-dev-runtime.js"), 'export * from "./jsx-runtime"'),
 
-    $build("index", { inject: { __FULL_BUILD__: "true" } }),
-    $build("index", { outputDir: OUT_DIR_MIN, inject: { __FULL_BUILD__: "false" } }),
-    $build("jsx-runtime", { inject: { "./jsx-dom": "jsx-dom", delimiters: ["", ""] } }),
-    $build("jsx-runtime", {
+    buildRollup("index", { inject: { __FULL_BUILD__: "true" } }),
+    buildRollup("index", { outputDir: OUT_DIR_MIN, inject: { __FULL_BUILD__: "false" } }),
+    buildRollup("jsx-runtime", {
+      inject: { "./jsx-dom": "jsx-dom", delimiters: ["", ""] },
+    }),
+    buildRollup("jsx-runtime", {
       outputDir: OUT_DIR_MIN,
       inject: { "./jsx-dom": "jsx-dom/min", delimiters: ["", ""] },
     }),
 
-    provideReexport("index.d.ts", OUT_DIR_MIN, "../index"),
-    provideReexport("jsx-runtime.d.ts", OUT_DIR_MIN, "./index", jsxRuntimeExports),
-    provideReexport("jsx-runtime.d.ts", OUT_DIR, "./index", jsxRuntimeExports),
+    reexport("index.d.ts", OUT_DIR_MIN, "../index"),
+    reexport("jsx-runtime.d.ts", OUT_DIR_MIN, "./index", jsxRuntimeExports),
+    reexport("jsx-runtime.d.ts", OUT_DIR, "./index", jsxRuntimeExports),
   ])
 }
 
