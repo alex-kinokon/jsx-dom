b33e9502c65046abe5c3cb1b5f955af17bda9747
proteriax <8125011+proteriax@users.noreply.github.com>
2021-04-12 21:18:43 -0400

    Add CJS build

--- scripts/build.ts
+++ scripts/build.ts
@@ -8,82 +8,95 @@ import node from "@rollup/plugin-node-resolve"
 import { rollup } from "rollup"
 import { formatSource } from "./format-source"
 
-const extensions = [".ts", ".js"]
-const jsxRuntimeExports = ["jsx", "jsxs", "Fragment", "JSX"]
-
-const OUT_DIR = resolve(__dirname, "../build")
-const OUT_DIR_MIN = resolve(OUT_DIR, "./min")
-
 interface BuildOptions {
+  targetDir: string
+  format: "cjs" | "esm"
+  packageName: string
+}
+
+interface RollupBuildOptions {
   input: string
   outputDir: string
   inject: RollupReplaceOptions
 }
 
-const buildRollup = async function (
-  name: string,
-  { outputDir = OUT_DIR, inject = {} }: Partial<BuildOptions>
-) {
-  const bundle = await rollup({
-    input: `./src/${name}.ts`,
-    external: ["jsx-dom", "jsx-dom/min"],
-    plugins: [
-      replace({ ...inject, preventAssignment: true }),
-      babel({
-        extensions,
-        comments: false,
-        minified: false,
-        babelHelpers: "bundled",
-        plugins: [
-          "@babel/plugin-transform-typescript",
-          "minify-constant-folding",
-          "minify-guarded-expressions",
-          "minify-dead-code-elimination",
-          ({ types: t }) => ({
-            visitor: {
-              CallExpression(path) {
-                if (t.isIdentifier(path.node.callee, { name: "cast" })) {
-                  path.replaceWith(path.node.arguments[0])
-                }
+export async function build({ targetDir, format, packageName }: BuildOptions) {
+  const isESM = format === "esm"
+
+  const extensions = [".ts", ".js"]
+  const jsxRuntimeExports = ["jsx", "jsxs", "Fragment", "JSX"]
+
+  const OUT_DIR = resolve(__dirname, "..", targetDir)
+  const OUT_DIR_MIN = resolve(OUT_DIR, "./min")
+
+  const buildRollup = async function (
+    name: string,
+    { outputDir = OUT_DIR, inject = {} }: Partial<RollupBuildOptions>
+  ) {
+    const bundle = await rollup({
+      input: `./src/${name}.ts`,
+      external: [packageName, `${packageName}/min`],
+      plugins: [
+        replace({ ...inject, preventAssignment: true }),
+        babel({
+          extensions,
+          comments: false,
+          minified: false,
+          babelHelpers: "bundled",
+          plugins: [
+            "@babel/plugin-transform-typescript",
+            "minify-constant-folding",
+            "minify-guarded-expressions",
+            "minify-dead-code-elimination",
+            ({ types: t }) => ({
+              visitor: {
+                CallExpression(path) {
+                  if (t.isIdentifier(path.node.callee, { name: "cast" })) {
+                    path.replaceWith(path.node.arguments[0])
+                  }
+                },
               },
-            },
-          }),
-        ],
-      }),
-      prettier({ tabWidth: 2, parser: "babel" }),
-      node({ extensions }),
-    ],
-  })
+            }),
+          ],
+        }),
+        prettier({ tabWidth: 2, parser: "babel" }),
+        node({ extensions }),
+      ],
+    })
 
-  await bundle.write({
-    format: "es",
-    file: `${outputDir}/${name}.js`,
-    exports: "named",
-  })
-}
+    await bundle.write({
+      format,
+      file: `${outputDir}/${name}.js`,
+      exports: "named",
+    })
+  }
 
-async function copyPackageJson() {
-  const source = await fs.readJSON(resolve(__dirname, "../package.json"))
-  delete source.devDependencies
-  delete source.private
-  delete source.scripts
-  delete source.prettier
-  source.type = "module"
-  await fs.writeJSON(resolve(OUT_DIR, "package.json"), source, { spaces: 2 })
-}
+  async function copyPackageJson() {
+    const source = await fs.readJSON(resolve(__dirname, "../package.json"))
+    source.name = packageName
+    delete source.devDependencies
+    delete source.private
+    delete source.scripts
+    delete source.prettier
+    if (isESM) {
+      source.type = "module"
+    } else {
+      delete source.module
+    }
+    await fs.writeJSON(resolve(OUT_DIR, "package.json"), source, { spaces: 2 })
+  }
 
-async function reexport(name: string, dest: string, src: string, namedExports?: string[]) {
-  const imports = namedExports ? `{${namedExports.join(",")}}` : "*"
-  const content = `export ${imports} from "${src}"`
+  async function reexport(name: string, dest: string, src: string, namedExports?: string[]) {
+    const imports = namedExports ? `{${namedExports.join(",")}}` : "*"
+    const content = `export ${imports} from "${src}"`
 
-  await fs.mkdirp(dest)
-  await fs.writeFile(resolve(dest, name), formatSource(content))
-}
+    await fs.mkdirp(dest)
+    await fs.writeFile(resolve(dest, name), formatSource(content))
+  }
 
-// https://github.com/jprichardson/node-fs-extra/issues/323
-const copy = (src: string, dest: string) => fs.copy(src, resolve(dest, src))
+  // https://github.com/jprichardson/node-fs-extra/issues/323
+  const copy = (src: string, dest: string) => fs.copy(src, resolve(dest, src))
 
-export async function build() {
   await fs.remove(OUT_DIR)
   await fs.ensureDir(OUT_DIR)
   await Promise.all([
@@ -92,16 +105,21 @@ export async function build() {
     copy("README.md", OUT_DIR),
     copy("CHANGELOG.md", OUT_DIR),
     copy("LICENSE", OUT_DIR),
-    fs.writeFile(resolve(OUT_DIR, "jsx-dev-runtime.js"), 'export * from "./jsx-runtime"'),
+    fs.writeFile(
+      resolve(OUT_DIR, "jsx-dev-runtime.js"),
+      format === "esm"
+        ? 'export * from "./jsx-runtime"'
+        : 'module.exports = require("./jsx-runtime")'
+    ),
 
     buildRollup("index", { inject: { __FULL_BUILD__: "true" } }),
     buildRollup("index", { outputDir: OUT_DIR_MIN, inject: { __FULL_BUILD__: "false" } }),
     buildRollup("jsx-runtime", {
-      inject: { "./jsx-dom": "jsx-dom", delimiters: ["", ""] },
+      inject: { "./jsx-dom": packageName, delimiters: ["", ""] },
     }),
     buildRollup("jsx-runtime", {
       outputDir: OUT_DIR_MIN,
-      inject: { "./jsx-dom": "jsx-dom/min", delimiters: ["", ""] },
+      inject: { "./jsx-dom": `${packageName}/min`, delimiters: ["", ""] },
     }),
 
     reexport("index.d.ts", OUT_DIR_MIN, "../index"),
@@ -110,5 +128,9 @@ export async function build() {
   ])
 }
 
-const args = process.argv.slice(2)
-exports[args[0]]()
+Promise.all([
+  build({ targetDir: "esm", format: "esm", packageName: "jsx-dom" }),
+  build({ targetDir: "cjs", format: "cjs", packageName: "jsx-dom-cjs" }),
+]).then(() => {
+  console.log("Done")
+})
